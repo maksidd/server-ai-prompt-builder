cabc10b790d4107fb583c3a2f03180a139089dc0
maksidd <mail.peshkov@gmail.com>
2025-02-20 13:34:26 +0200

    Update Template.php

--- models/Template.php
+++ models/Template.php
@@ -16,13 +16,6 @@ class Template
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    public function getTemplate($id)
-    {
-        $stmt = $this->pdo->prepare("SELECT * FROM templates WHERE id = :id");
-        $stmt->execute(['id' => $id]);
-        return $stmt->fetch(PDO::FETCH_ASSOC);
-    }
-
     public function createTemplate($data)
     {
         $stmt = $this->pdo->prepare("INSERT INTO templates (title, description, content, tags, rating) VALUES (:title, :description, :content, :tags, :rating)");
@@ -34,19 +27,6 @@ class Template
             'rating' => $data['rating']
         ]);
         return $this->pdo->lastInsertId();
-    }
-
-    public function updateTemplate($id, $data)
-    {
-        $fields = [];
-        foreach ($data as $key => $value) {
-            $fields[] = "$key = :$key";
-        }
-        $query = "UPDATE templates SET " . implode(', ', $fields) . " WHERE id = :id";
-        $stmt = $this->pdo->prepare($query);
-        $data['id'] = $id;
-        $stmt->execute($data);
-        return $stmt->rowCount();
     }
 
     public function deleteTemplate($id)
