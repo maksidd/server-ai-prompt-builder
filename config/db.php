48a89ff974a66a40f555aac101f0b09c64cacc66
maksidd <mail.peshkov@gmail.com>
2025-02-22 14:16:26 +0200

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,14 +1,22 @@
 <?php
 // config/db.php
 
-$host = 'localhost';
-$dbname = 'your_database_name';
-$user = 'your_database_user';
-$password = 'your_database_password';
+
+$host = 'crossover.proxy.rlwy.net'; // Хост из Railway (через прокси)
+$port = '45546';                     
+$dbname = 'railway';                 
+$user = 'postgres';                  // Имя пользователя (из строки подключения)
+$password = 'ggiacPLtVMbXVGDDOglcDbQnBetsLZZX'; // Пароль из Railway
+
 
 try {
-    $pdo = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname;sslmode=require";
+    $pdo = new PDO($dsn, $user, $password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+    echo "Успешное подключение к базе данных!";
+
+    //$pdo = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
+    //$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Database connection failed: " . $e->getMessage());
 }
